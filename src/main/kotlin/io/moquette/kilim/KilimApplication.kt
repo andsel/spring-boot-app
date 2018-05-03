51730dbd8e79bcfc64ee305bb2e3f718c6b2857e
andsel <selva.andre@gmail.com>
2018-05-03 18:39:35 +0200

    Initial commit

---
+++ src/main/kotlin/io/moquette/kilim/KilimApplication.kt
@@ -0,0 +1,11 @@
+package io.moquette.kilim
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KilimApplication
+
+fun main(args: Array<String>) {
+    runApplication<KilimApplication>(*args)
+}
