f250c67157c99aded1185a20d2066d187738e4d2
andsel <selva.andre@gmail.com>
2018-05-29 22:25:23 +0200

    Introduces React.js for device's messages

---
+++ src/main/js/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+
+var root = document.getElementById('react');
+ReactDOM.render(
+//  <MainPanel {...(root.dataset)}/>,
+<label> ciaone </label>,
+  root
+);
