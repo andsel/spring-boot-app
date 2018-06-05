f5609a608b30aacffa171b87c344d36ddb9b4299
andsel <selva.andre@gmail.com>
2018-06-05 22:29:26 +0200

    Switched from RestTemplate to reactive WebClient for HTTP requests to Telegram

--- src/main/kotlin/io/moquette/kilim/infrastructure/TelegramBotService.kt
+++ src/main/kotlin/io/moquette/kilim/infrastructure/TelegramBotService.kt
@@ -3,8 +3,10 @@ package io.moquette.kilim.infrastructure
 import io.moquette.kilim.model.IAdminNotificator
 import io.moquette.kilim.model.User
 import org.slf4j.LoggerFactory
+import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestTemplate
+import org.springframework.web.reactive.function.client.WebClient
 
 @Component
 class TelegramBotService : IAdminNotificator {
@@ -15,16 +17,21 @@ class TelegramBotService : IAdminNotificator {
         val urlVariables = hashMapOf("botToken" to "391251051:AAE6NhJTpEwOVWxx1wI1baUbZbIVPLoGsCY",
                 "groupId" to "-253056505",
                 "message" to "New registration request from ${user.login}")
-        val restTemplate = RestTemplate()
-        val response = restTemplate.getForObject(
-                "https://api.telegram.org/bot{botToken}/sendMessage?chat_id={groupId}&text={message}",
-                TelegramResponse::class.java,
-                urlVariables)!!
-        if (response.ok) {
-            LOG.debug("Successfully notified")
-        } else {
-            LOG.warn("Some bad messages happened")
-        }
+
+        WebClient.create("https://api.telegram.org")
+                .get()
+                .uri("/bot{botToken}/sendMessage?chat_id={groupId}&text={message}", urlVariables)
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToMono(TelegramResponse::class.java)
+                .doOnError { _ -> LOG.warn("Request to telegram gone bad")}
+                .subscribe { response ->
+                    if (response.ok) {
+                        LOG.debug("Successfully notified")
+                    } else {
+                        LOG.warn("Some bad messages happened")
+                    }
+                }
     }
 }
 
